bc0b056798ed62120f3b2e299f68103cce4cec40
saket <saketj420@gmail.com>
2021-06-07 12:44:43 +0530

    Edit contact successfully

--- JS/contact.js
+++ JS/contact.js
@@ -49,67 +49,86 @@ window.addEventListener('DOMContentLoaded', (event) => {
     checkForUpdate();
 });
 
-const save = () => {
-    try{
-    let addressBookData = createAddressBook();
-    createAndupdateStorage(addressBookData);
-    document.location.replace(site_properties.home_page);
-    }catch (e){
+const save = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    try {
+        setAddressBookObject();
+        createAndupdateStorage();
+        resetForm();
+        window.location.replace(site_properties.home_page);
+    } catch (e) {
         return;
+    }
+    //alert(addressBookObj.toString());
+}
+
+const setAddressBookObject = () => {
+    try {
+        
+        addressBookObj._name = document.querySelector('#name').value;
+        addressBookObj._phoneNo = document.querySelector('#mobile').value;
+        addressBookObj._address = document.querySelector('#address').value;
+        addressBookObj._city = document.querySelector('#city').value;
+        addressBookObj._state = document.querySelector('#state').value;
+        addressBookObj._zipCode = document.querySelector('#zip').value;
+    } catch (e) {
+        alert("Please enter valid details!");
     }
 }
 
-const createAddressBook = () =>{
+const createNewConatctId = () => {
+    let personID = localStorage.getItem("ContactID");
+    personID = !personID ? 1 : (parseInt(personID) + 1).toString();
+    localStorage.setItem("ContactID", personID);
+    return personID;
+}
+
+const createAndupdateStorage = () => {
+    let addressBookList = JSON.parse(localStorage.getItem('AddressBookList'));
+    if (addressBookList) {
+        let addressData = addressBookList.find(address => address._id == addressBookObj._id);
+        if (!addressData) {
+            addressBookList.push(createAddressBookData());
+        } else {
+            const index = addressBookList.map(address => address._id)
+                                         .indexOf(addressData._id);
+            addressBookList.splice(index, 1, createAddressBookData(addressData._id));
+        }
+    } else {
+        addressBookList = [createAddressBookData()];
+    }
+    // alert("Local Contact Details: "+addressBookObj.toString());
+    localStorage.setItem('AddressBookList', JSON.stringify(addressBookList));
+}
+
+const createAddressBookData = (id) => {
     let addressBookData = new AddressBook();
+    if (!id) addressBookData.id = createNewConatctId();
+    else addressBookData.id = id;
+    setAddressBookData(addressBookData);
+    return addressBookData;
+}
+
+const setAddressBookData = (addressBookData) => {
     try {
-        addressBookData.name = getInputValueById('#name');
+        addressBookData.name = addressBookObj._name;
     } catch (e) {
         setTextValue('.error-text-output', e);
         throw e;
     }
-    addressBookData.id = new Date().getTime() +1;
-    addressBookData.phoneNo = getInputValueById('#mobile');
-    addressBookData.address = getInputValueById('#address');
-    let city = document.querySelector('#city').value;
-    addressBookData.city = city;
-    let state = document.querySelector('#state').value;
-    addressBookData.state = state;
-    addressBookData.zipCode = getInputValueById('#zip');
-    alert("Contact Details: "+addressBookData.toString());
-    return addressBookData;
-}
-
-const getSelectedValues = (propertyValue) => {
-    let allItems = document.querySelectorAll(propertyValue);
-    let selItems = [];
-    allItems.forEach(item => {
-        if (item.checked) selItems.push(item.value);
-    });
-    return selItems;
-}
-
-
-const getInputValueById = (id) => {
-    let value = document.querySelector(id).value;
-    return value;
-}
-
-function createAndupdateStorage(addressBookData) {
-    let addressBookList = JSON.parse(localStorage.getItem('AddressBookList'));
-
-    if (addressBookList != undefined) {
-        addressBookList.push(addressBookData);
-    } else {
-        addressBookList = [addressBookData];
-    }
-    alert("Local Contact Details: "+addressBookList.toString());
-    localStorage.setItem('AddressBookList', JSON.stringify(addressBookList));
+    addressBookData.phoneNo = addressBookObj._phoneNo;
+    addressBookData.address = addressBookObj._address;
+    addressBookData.city = addressBookObj._city;
+    addressBookData.state = addressBookObj._state;
+    addressBookData.zipCode = addressBookObj._zipCode;
+    alert(addressBookData.toString());
 }
 
 const checkForUpdate = () => {
     const addressBookJson = localStorage.getItem('editContact');
     isUpdate = addressBookJson ? true : false;
-    if(!isUpdate) return;
+    if (!isUpdate) return;
     addressBookObj = JSON.parse(addressBookJson);
     setForm();
 }
@@ -123,19 +142,33 @@ const setForm = () => {
     document.querySelector('#zip').value = addressBookObj._zipCode;
 }
 
+function resetForm() {
+    document.querySelector("#name").value = "";
+    document.querySelector('#mobile').value = "";
+    document.querySelector('#address').value = "";
+    document.querySelector('#city').value = "";
+    document.querySelector('#state').value = "";
+    document.querySelector('#zip').value = "";
+}
+
 const setValue = (id, value) => {
     const element = document.querySelector(id);
     element.value = value;
 }
+const setTextValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.value = value;
+}
+
 
 const setSelectedValue = (propertyValue, value) => {
     let allItems = document.querySelectorAll(propertyValue);
     allItems.forEach(item => {
-        if(value.includes(item.values)){
+        if (value.includes(item.values)) {
             item.checked = true;
         }
         else if (item.value === value)
-        item.checked = true;
+            item.checked = true;
     });
 }
 
--- JS/site_properties.js
+++ JS/site_properties.js
@@ -1,5 +1,4 @@
 let site_properties = {
     home_page: "../Pages/home.html",
     AddressBook_page: "../Pages/AddressBook.html"
-
 };
